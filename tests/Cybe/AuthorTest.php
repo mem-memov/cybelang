e53dd88d705614304749a9c0e4898ca80f2804c9
Alexander Novikov <a.novikov@digsolab.com>
2017-02-02 16:55:25 +0300

    author writes

---
+++ tests/Cybe/AuthorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MemMemov\Cybe;
+
+class AuthorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItWritesMessage()
+    {
+        $author = new Author();
+    }
+}
