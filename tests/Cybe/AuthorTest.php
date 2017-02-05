692ed9481b66dd3ca2e64ac4e4c95604da8adc20
Alexander Novikov <mem-memov@yandex.ru>
2017-02-05 13:05:20 +0300

    author writes message

--- tests/Cybe/AuthorTest.php
+++ tests/Cybe/AuthorTest.php
@@ -4,27 +4,48 @@ namespace MemMemov\Cybe;
 
 class AuthorTest extends \PHPUnit_Framework_TestCase
 {
-    protected $clauses;
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $parser;
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $messages;
 
     protected function setUp()
     {
-        $this->clauses = $this->getMockBuilder(Clauses::class)
+        $this->parser = $this->getMockBuilder(Parser\Messages::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->messages = $this->getMockBuilder(Clauses::class)
             ->disableOriginalConstructor()
             ->getMock();
     }
 
     public function testItWritesMessage()
     {
-        $author = new Author($this->clauses);
+        $author = new Author($this->parser, $this->messages);
 
         $text = 'врач.ставить(что:диагноз,кому:больной,когда:сейчас)';
         $text .= 'врач.лечить(кого:больной,отчего:болезнь,когда:врач.ставить.после)';
         $text .= 'лекарство.есть(какой:хороший)';
         $text .= 'врач.дать(что:лекарство,кому:больной)';
 
+        $messageText = $this->getMockBuilder(Parser\Message::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->parser->expects($this->once())
+            ->method('create')
+            ->with($text)
+            ->willReturn($messageText);
+
+        $message = $this->getMockBuilder(Message::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->clauses->expects($this->once())
             ->method('fromText')
-            ->with($text);
+            ->with($messageText)
+            ->willReturn($message);
 
         $author->write($text);
     }
