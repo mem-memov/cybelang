f62a7ba0bbd2a89b3a24816f2cc04dbfa9846ca1
Alexander Novikov <mem-memov@yandex.ru>
2017-02-05 15:47:34 +0300

    clause from text

--- src/Cybe/Clause.php
+++ src/Cybe/Clause.php
@@ -4,5 +4,17 @@ namespace MemMemov\Cybe;
 
 class Clause
 {
+    private $predicate;
+    private $subject;
+    private $arguments;
 
+    public function __construct(
+        Predicate $predicate,
+        Subject $subject,
+        array $arguments
+    ) {
+        $this->predicate = $predicate;
+        $this->subject = $subject;
+        $this->arguments = $arguments;
+    }
 }
--- tests/Cybe/ClausesTest.php
+++ tests/Cybe/ClausesTest.php
@@ -4,5 +4,107 @@ namespace MemMemov\Cybe;
 
 class ClausesTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $predicates;
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $subjects;
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    protected $arguments;
 
+    protected function setUp()
+    {
+        $this->predicates = $this->getMockBuilder(Predicates::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->subjects = $this->getMockBuilder(Subjects::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->arguments = $this->getMockBuilder(Arguments::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testItCreatesClauseFromText()
+    {
+        $clauses = new Clauses($this->predicates, $this->subjects, $this->arguments);
+
+        $clauseText = $this->getMockBuilder(Parser\Clause::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // predicate
+
+        $predicateText = $this->getMockBuilder(Parser\Predicate::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $clauseText->expects($this->once())
+            ->method('predicate')
+            ->willReturn($predicateText);
+
+        $predicate = $this->getMockBuilder(Predicate::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->predicates->expects($this->once())
+            ->method('fromText')
+            ->with($predicateText)
+            ->willReturn($predicate);
+
+        // subject
+
+        $subjectText = $this->getMockBuilder(Parser\Subject::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $clauseText->expects($this->once())
+            ->method('subject')
+            ->willReturn($subjectText);
+
+        $subject = $this->getMockBuilder(Subject::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->subjects->expects($this->once())
+            ->method('fromText')
+            ->with($subjectText)
+            ->willReturn($subject);
+
+        // arguments
+
+        $argumentText_1 = $this->getMockBuilder(Parser\Argument::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $argumentText_2 = $this->getMockBuilder(Parser\Argument::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $clauseText->expects($this->once())
+            ->method('arguments')
+            ->with()
+            ->willReturn([$argumentText_1, $argumentText_2]);
+
+        $argument_1 = $this->getMockBuilder(Argument::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $argument_2 = $this->getMockBuilder(Argument::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->arguments->expects($this->exactly(2))
+            ->method('fromText')
+            ->withConsecutive(
+                [$argumentText_1],
+                [$argumentText_2]
+            )
+            ->will($this->onConsecutiveCalls($argument_1, $argument_2));
+
+        $result = $clauses->fromText($clauseText);
+
+        $this->assertInstanceOf(Clause::class, $result);
+    }
 }
